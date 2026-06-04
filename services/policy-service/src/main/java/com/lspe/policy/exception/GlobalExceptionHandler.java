729447a4f904cd87a1e409cca4a368eb186f6377
shubhojit-mitra-dev <Shubhojit.120225@stu.upes.ac.in>
2026-06-04 22:56:04 +0530

    feat: add GlobalExceptionHandler to handle ResourceNotFoundException globally

---
+++ services/policy-service/src/main/java/com/lspe/policy/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.lspe.policy.exception;
+
+import com.lspe.common.exception.ResourceNotFoundException;
+import com.lspe.common.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ApiResponse.error(ex.getMessage()));
+    }
+}
