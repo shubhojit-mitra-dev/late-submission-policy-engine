f76a1c4b26c1a5e60f291dbe8d5b1b1af5987947
shubhojit-mitra-dev <Shubhojit.120225@stu.upes.ac.in>
2026-06-05 11:53:00 +0530

    feat: add SubmissionResponse and ResultResponse DTOs for submission evaluation data

---
+++ services/submission-service/src/main/java/com/lspe/submission/dto/response/ResultResponse.java
@@ -0,0 +1,25 @@
+package com.lspe.submission.dto.response;
+
+import com.lspe.common.enums.SubmissionStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResultResponse {
+    private String id;
+    private String policyVersionId;
+    private Double rawScore;
+    private Double penaltyApplied;
+    private Double latenessHours;
+    private Double effectiveLatenessHours;
+    private Double finalScore;
+    private SubmissionStatus status;
+    private LocalDateTime evaluatedAt;
+}
